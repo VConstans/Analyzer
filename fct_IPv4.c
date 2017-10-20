5ebd2f492692f192a70eb37b5266891b4b9e339d
Victor <victor.constans@etu.unistra.fr>
2017-10-20 17:27:53 +0200

    Add print IP adresses function

--- fct_IPv4.c
+++ fct_IPv4.c
@@ -1,43 +1,52 @@
 #include "fct_IPv4.h"
 
+#define IP_ALEN 4
+
 void printIPv4Addr(u_int32_t addr)
 {
+//	addr = ntohl(addr);
 	int i;
-	char octet;
+	unsigned char octet;
 
-	for(i=0;i<4;i++)
+	for(i=0;i<IP_ALEN;i++)
 	{
-		octet=(char)addr;
-		printf("%d.",octet);
-		addr>>8;
+		octet=(unsigned char)addr;
+		printf("%d",octet);
+
+		if(i<IP_ALEN-1)
+		{
+			printf(".");
+		}
+
+		addr = addr>>8;
 	}	
 }
 
-void treatIPv4(struct ip* entete)
+void treatIPv4(struct iphdr* entete)
 {
-	printf("Version %d\n",entete->ip_v);
+	printf("Version %d\n",entete->version);
 
-	int taille_entete = entete->ip_hl;
+	int taille_entete = entete->ihl;
 	printf("IHL %d\n",taille_entete);
 
 
-	printf("TOS %x\n", entete->ip_tos);
+	printf("TOS %x\n", entete->tos);
 
-	printf("Total length %d\n",entete->ip_len);
+	printf("Total length %d\n",entete->tot_len);
 
-	printf("Identification %x\n",entete->ip_id);
+	printf("Identification %x\n",entete->id);
 
-	printf("Fragment Offset %x\n",entete->ip_off);
+	printf("Fragment Offset %x\n",entete->frag_off);
 
-	printf("TTL %d\n",entete->ip_ttl);
+	printf("TTL %d\n",entete->ttl);
 
-	printf("Protocol %x\n",entete->ip_p);
+	printf("Protocol %x\n",entete->protocol);
 
-	printf("Checksum %x\n",entete->ip_sum);
+	printf("Checksum %x\n",entete->check);
 
 	printf("Adresse source ");
 
-//	printIPv4Addr(entete->saddr);
+	printIPv4Addr(entete->saddr);
 
 	printf("\n");
 }
--- fct_IPv4.h
+++ fct_IPv4.h
@@ -2,8 +2,11 @@
 #define __fct_IPv4_h__
 
 #include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 
 
-void treatIPv4(struct ip* entete);
+void treatIPv4(struct iphdr* entete);
 
 #endif
