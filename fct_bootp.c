c38423dde82954f6e31cbbeefbf6277bb9cf0f4d
Victor <victor.constans@etu.unistra.fr>
2017-10-24 00:10:13 +0200

    Begin to treat bootp header

--- fct_bootp.c
+++ fct_bootp.c
@@ -2,5 +2,10 @@
 
 void treatBootp(void* entete)
 {
-	
+	struct bootp* enteteB = (struct bootp*)entete;
+
+	if(*((unsigned long*)(enteteB->bp_vend)) == 0x63825663)
+	{
+		printf("Bootp\n");
+	}
 }
--- fct_bootp.h
+++ fct_bootp.h
@@ -1,6 +1,10 @@
 #ifndef __FCT_BOOTP_H__
 #define __FCT_BOOTP_H__
 
+#include "netinet/bootp.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 void treatBootp(void* entete);
 
 #endif
