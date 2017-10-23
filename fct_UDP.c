63e75160f59ec5ff7128c0476e1a8f76b85d4500
Victor <victor.constans@etu.unistra.fr>
2017-10-24 00:09:48 +0200

    Correct endianness bug of UDP

--- fct_UDP.c
+++ fct_UDP.c
@@ -4,22 +4,27 @@ void treatUDP(void* entete)
 {
 	struct udphdr* enteteUDP = (struct udphdr*)entete;
 
-	printf("Port source %u\n",enteteUDP->source);
-	printf("Port destination %u\n",enteteUDP->dest);
-	printf("Longueur %u\n",enteteUDP->len);
-	printf("Checksum %u\n",enteteUDP->check);
+	u_int16_t source = ntohs(enteteUDP->source);
+	u_int16_t destination = ntohs(enteteUDP->dest);
+	u_int16_t lg = ntohs(enteteUDP->len);
+	u_int16_t check = ntohs(enteteUDP->check);
+
+	printf("Port source %d\n",source);
+	printf("Port destination %d\n",destination);
+	printf("Longueur %d\n",lg);
+	printf("Checksum %x\n",check);
 
 
 	void* enteteNiv7 = entete + sizeof(struct udphdr);
 
 	//BOOTP
-	if(enteteUDP->source == 67 || enteteUDP->dest == 67 || enteteUDP->source == 68 || enteteUDP->dest == 68)
+	if(source == 67 || destination == 67 || source == 68 || destination == 68)
 	{
 		treatBootp(enteteNiv7);
 	}
 
 	//DNS
-	if(enteteUDP->source == 53 || enteteUDP->dest == 53)
+	if(source == 53 || destination == 53)
 	{
 		//treatDNS(enteteNiv7);
 	}
