46286e0215c21002ad5542fd413d51aa353e0d67
Victor <victor.constans@etu.unistra.fr>
2018-01-02 01:17:35 +0100

    Doesn't print data if they are encoding or the type of the content is not a text

--- include/fct_http.h
+++ include/fct_http.h
@@ -2,6 +2,7 @@
 #define __FCT_HTTP_H__
 
 #include "utile.h"
+#include <string.h>
 
 void treatHTTP(void*,int);
 
--- src/fct_http.c
+++ src/fct_http.c
@@ -4,7 +4,52 @@ void treatHTTP(void* entete, int len)
 {
 	printf("=====================>HTTP\n");
 
-
 	u_int8_t* httpPayload = (u_int8_t*)entete;
-	printHexToAscii_len(len,httpPayload);
+
+	if(!memcmp(httpPayload,"HTTP",4))
+	{
+		printf("Reply\n");
+	}
+	else if(!memcmp(httpPayload,"GET",3) || !memcmp(httpPayload,"POST",4) || !memcmp(httpPayload,"HEAD",4))
+	{
+		printf("Request ");
+	}
+
+	int i;
+	int affiche = 1;
+	for(i=0;i<len;i++)
+	{
+		if(httpPayload[i] == '\n')
+		{
+			if(!memcmp(&httpPayload[i+1],"Content-Type: ",14))
+			{
+				if(memcmp(&httpPayload[i+15],"text",4)!= 0)
+				{
+					affiche = 0;
+				}
+			}
+			if(!memcmp(&httpPayload[i+1],"Content-Encoding: ",18))
+			{
+				affiche = 0;
+			}
+		}
+
+		if(httpPayload[i] == '\r')
+		{
+			if(!memcmp(&httpPayload[i],"\r\n\r\n",4))
+			{
+				break;
+			}
+		}
+
+		printf("%c",httpPayload[i]);
+	}
+
+	if(affiche)
+	{
+		for(;i<len;i++)
+		{
+			printf("%c",httpPayload[i]);
+		}
+	}
 }
