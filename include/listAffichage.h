23f5b52731ce55a5f5bcd89adb3d234955dfc9b2
Victor <victor.constans@etu.unistra.fr>
2018-01-05 18:58:20 +0100

    Add listAffichage

---
+++ include/listAffichage.h
@@ -0,0 +1,18 @@
+#ifndef __LIST_AFFICHAGE_H__
+#define __LIST_AFFICHAGE_H__
+
+#define SRC 0
+#define DST 1
+
+struct listAffichage
+{
+	u_int16_t port;
+	int sens;
+	int affichage;
+	struct listAffichage* suivant;
+};
+
+struct listAffichage* getAffichage(struct listAffichage*,u_int16_t,int);
+struct listAffichage* setAffichage(struct listAffichage*,u_int16_t,int,int);
+
+#endif
