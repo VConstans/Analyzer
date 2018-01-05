9e984f334e9c9ae125f4e9d4d7d5e2673961ed23
Victor <victor.constans@etu.unistra.fr>
2018-01-05 02:13:22 +0100

    Doesn't print media encoding in http reply

--- src/fct_http.c
+++ src/fct_http.c
@@ -8,51 +8,74 @@ void treatHTTP(void* entete, int len)
 	printf("=====================>HTTP\n");
 
 	u_int8_t* httpPayload = (u_int8_t*)entete;
+	int enteteHTTP = 0;
 
 	if(!memcmp(httpPayload,"HTTP",4))
 	{
 		printf("Reply\n");
+		enteteHTTP = 1;
 	}
 	else if(!memcmp(httpPayload,"GET",3) || !memcmp(httpPayload,"POST",4) || !memcmp(httpPayload,"HEAD",4))
 	{
 		printf("Request ");
+		enteteHTTP = 1;
 	}
 
-	int i;
-	int affiche = 1;
-	for(i=0;i<len;i++)
+
+	int i = 0;
+	int affiche = 0;
+	if(enteteHTTP)
 	{
-		if(httpPayload[i] == '\n')
+		affiche = 1;
+		for(i=0;i<len;i++)
 		{
-			if(!memcmp(&httpPayload[i+1],"Content-Type: ",14))
+			if(httpPayload[i] == '\n')
 			{
-				if(memcmp(&httpPayload[i+15],"text",4)!= 0)
+				if(!memcmp(&httpPayload[i+1],"Content-Type: ",14))
+				{
+					if(memcmp(&httpPayload[i+15],"text",4)!= 0)
+					{
+						affiche = 0;
+					}
+				}
+				if(!memcmp(&httpPayload[i+1],"Content-Encoding: ",18))
 				{
 					affiche = 0;
 				}
+/*
+				if(!memcmp(&httpPayload[i+1],"Content-Length: ",16))
+				{
+					int j=i+17;
+					while(httpPayload[j] != '\r')
+					{
+						j++;
+					}
+					httpPayload[j] = '\0';
+					int length = atoi(httpPayload[i+17]);
+					setLength(listLength,id,length);
+					httpPayload[j] = '\r';
+				}*/
 			}
-			if(!memcmp(&httpPayload[i+1],"Content-Encoding: ",18))
-			{
-				affiche = 0;
-			}
-		}
 
-		if(httpPayload[i] == '\r')
-		{
-			if(!memcmp(&httpPayload[i],"\r\n\r\n",4))
+			if(httpPayload[i] == '\r')
 			{
-				break;
+				if(!memcmp(&httpPayload[i],"\r\n\r\n",4))
+				{
+					break;
+				}
 			}
-		}
 
-		printf("%c",httpPayload[i]);
+			printf("%c",httpPayload[i]);
+		}
 	}
 
+//	struct listLength* maillonLength = getLength(listLength,id);
 	if(affiche)	//TODO  tenir compte de content len
 	{
 		for(;i<len;i++)
 		{
-			printf("%c",httpPayload[i]);
+				printf("%c",httpPayload[i]);
 		}
+//		maillonLength->length--;
 	}
 }
--- src/fct_tcp.c
+++ src/fct_tcp.c
@@ -67,7 +67,7 @@ void treatTCP(void* entete,int len)
 
 	len -= 4*hdrLen;
 
-	if(len > 0 && enteteTCP->psh)
+	if(len > 0 /*&& enteteTCP->psh*/)
 		{
 
 		if(source == 80 || destination == 80)
