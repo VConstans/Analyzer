cf01e7a79447da3a484d18f2855a605f2305efb1
Victor <victor.constans@etu.unistra.fr>
2018-01-08 10:10:02 +0100

    Printing message according to the level of verbose for telnet

--- src/fct_telnet.c
+++ src/fct_telnet.c
@@ -6,8 +6,19 @@ void treatTelnet(void* entete,int len)
 	extern int verbose;
 	levelPrinting = 3;
 
-	printLevelLayer();
-	printf("Telnet:\n");
+	if( verbose == 3)
+	{
+		printLevelLayer();
+	}
+	printf("Telnet:");
+	if(verbose == 3)
+	{
+		printf("\n");
+	}
+	else
+	{
+		printf("\t");
+	}
 
 	u_int8_t* message = (u_int8_t*)entete;
 
@@ -16,39 +27,39 @@ void treatTelnet(void* entete,int len)
 	{
 		if(message[i] == IAC)
 		{
-			printLevelLayer();
-			i++;
-			switch (message[i])
+			if(verbose == 3)
 			{
-				case NOP:
-					printf("NOP (no operation)\n");
-					break;
-;				case DM:
-					printf("Data Mark\n");
-					break;
-				case IP:
-					printf("Interrupt Process\n");
-					break;
-				case AO:
-					printf("Abort Output\n");
-					break;
-				case AYT:
-					printf("Are You There\n");
-					break;
-				case EC:
-					printf("Erase Character\n");
-					break;
-				case EL:
-					printf("Erase Line\n");
-					break;
-				case GA:
-					printf("Go Ahead\n");
-					break;
-				case SB:
-					printf("Suboption ");
-					i++;
-				//	while(message[i] != SE)
-				//	{
+				printLevelLayer();
+				i++;
+				switch (message[i])
+				{
+					case NOP:
+						printf("NOP (no operation)\n");
+						break;
+	;				case DM:
+						printf("Data Mark\n");
+						break;
+					case IP:
+						printf("Interrupt Process\n");
+						break;
+					case AO:
+						printf("Abort Output\n");
+						break;
+					case AYT:
+						printf("Are You There\n");
+						break;
+					case EC:
+						printf("Erase Character\n");
+						break;
+					case EL:
+						printf("Erase Line\n");
+						break;
+					case GA:
+						printf("Go Ahead\n");
+						break;
+					case SB:
+						printf("Suboption ");
+						i++;
 						switch(message[i])
 						{
 							case TT:
@@ -147,62 +158,62 @@ void treatTelnet(void* entete,int len)
 								break;
 						}
 						i++;
-				//	}
-					break;
-				case WILL:
-					printf("WILL ");
-					break;
-				case WONT:
-					printf("WON'T ");
-					break;
-				case DO:
-					printf("DO ");
-					break;
-				case DONT:
-					printf("DON'T ");
-					break;
-				case SE:
-					printf("Suboption End\n");
-					break;
-				default:
-					printf("Option non reconnu\n");
-			}
-
-			if(message[i] == WILL || message[i] == WONT || message[i] == DO || message[i] == DONT)
-			{
-				i++;
-				switch(message[i])
-				{
-					case ECHO:
-						printf("Echo\n");
 						break;
-					case SGA:
-						printf("Suppress Go Ahead\n");
+					case WILL:
+						printf("WILL ");
 						break;
-					case TT:
-						printf("Terminal Type\n");
+					case WONT:
+						printf("WON'T ");
 						break;
-					case WS:
-						printf("Window Size\n");
+					case DO:
+						printf("DO ");
 						break;
-					case TS:
-						printf("Terminal Speed\n");
+					case DONT:
+						printf("DON'T ");
 						break;
-					case LM:
-						printf("Line Mode\n");
-						break;
-					case EV:
-						printf("Environment Variables\n");
-						break;
-					case NEV:
-						printf("New Environment Variables\n");
-						break;
-					case XDL:
-						printf("X Display Location\n");
+					case SE:
+						printf("Suboption End\n");
 						break;
 					default:
-						printf("Option non reconu\n");
-						break;
+						printf("Option non reconnu\n");
+				}
+
+				if(message[i] == WILL || message[i] == WONT || message[i] == DO || message[i] == DONT)
+				{
+					i++;
+					switch(message[i])
+					{
+						case ECHO:
+							printf("Echo\n");
+							break;
+						case SGA:
+							printf("Suppress Go Ahead\n");
+							break;
+						case TT:
+							printf("Terminal Type\n");
+							break;
+						case WS:
+							printf("Window Size\n");
+							break;
+						case TS:
+							printf("Terminal Speed\n");
+							break;
+						case LM:
+							printf("Line Mode\n");
+							break;
+						case EV:
+							printf("Environment Variables\n");
+							break;
+						case NEV:
+							printf("New Environment Variables\n");
+							break;
+						case XDL:
+							printf("X Display Location\n");
+							break;
+						default:
+							printf("Option non reconu\n");
+							break;
+					}
 				}
 			}
 		}
