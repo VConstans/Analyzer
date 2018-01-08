034119eb2dcd59a2c2b4a26ae26934e3f4318529
Victor <victor.constans@etu.unistra.fr>
2018-01-08 09:55:24 +0100

    Printing message according to level of verbose for ftp

--- src/fct_ftp.c
+++ src/fct_ftp.c
@@ -6,46 +6,81 @@ void treatFTP(void* entete, int len)
 	extern int verbose;
 	levelPrinting = 3;
 
-	printLevelLayer();
-	printf("FTP:\n");
+	if(verbose >= 2)
+	{
+		if(verbose == 3)
+		{
+			printLevelLayer();
+		}
+		printf("FTP:");
+		if(verbose ==3)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("\t");
+		}
+	}
 
 	u_int8_t* messageFTP = (u_int8_t*)entete;
 
 	if(*messageFTP >= 0x30 && *messageFTP <=0x39)
 	{
-		printLevelLayer();
-		printf("Response\n");
-		printLevelLayer();
-		printf("Response code: ");
+		if(verbose == 3)
+		{
+			printLevelLayer();
+		}
+		printf("Response ");
+		if(verbose == 3)
+		{
+			printLevelLayer();
+		}
 		printf("%c",messageFTP[0]);
 		printf("%c",messageFTP[1]);
 		printf("%c",messageFTP[2]);
-		printf("\n");
-
-
-		printLevelLayer();
-		if(messageFTP[3] != 0x20)
+		if(verbose == 3)
 		{
-			printf("Error ftp format message\n");
+			printf("\n");
+			printLevelLayer();
 		}
-		else
-		{
-			u_int8_t* curseur = &messageFTP[4];
 
-			int i;
-			for(i=0;i<len-4;i++)
+
+		if(verbose == 3)
+		{		
+			if(messageFTP[3] != 0x20)
 			{
-				printf("%c",*curseur);
-				curseur++;
+				printf("Error ftp format message\n");
+			}
+			else
+			{
+				u_int8_t* curseur = &messageFTP[4];
+
+				int i;
+				for(i=0;i<len-4;i++)
+				{
+					printf("%c",*curseur);
+					curseur++;
+				}
 			}
 		}
 	}
 	else
 	{
-		printLevelLayer();
-		printf("Request\n");
-		printLevelLayer();
-		printf("Request Command: ");
+		if(verbose == 3)
+		{
+			printLevelLayer();
+		}
+		printf("Request");
+		if(verbose ==3)
+		{
+			printf("\n");
+			printLevelLayer();
+		}
+		else
+		{
+			printf("\t");
+		}
 
 		while(*messageFTP != 0x20)
 		{
@@ -60,14 +95,24 @@ void treatFTP(void* entete, int len)
 			}
 		}
 		len--;
-		printf("\n");
-
-		int i;
-		printLevelLayer();
-		for(i=0;i<len;i++)
+		if(verbose == 3)
 		{
-			printf("%c",*messageFTP);
-			messageFTP++;
+			printf("\n");
+		}
+		else
+		{
+			printf("\t");
+		}
+
+		if(verbose == 3)
+		{
+			int i;
+			printLevelLayer();
+			for(i=0;i<len;i++)
+			{
+				printf("%c",*messageFTP);
+				messageFTP++;
+			}
 		}
 	}
 }
